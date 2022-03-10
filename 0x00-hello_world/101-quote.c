0d8c7dac7017b41ed1e30a761879a75861bc7245
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-03-10 14:34:21 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

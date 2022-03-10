8e18c65e791b74f1cced964a8b53170d18236450
phantware <phantmoney2011@gmail.com>
2022-03-10 10:20:05 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,18 +2,9 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * - Dora Korpar, 2015-10-19",
- * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
- */
+ * main - prints exactly 
 int main(void)
 {
-<<<<<<< HEAD
-write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-=======
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
->>>>>>> 0d8c7dac7017b41ed1e30a761879a75861bc7245
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }

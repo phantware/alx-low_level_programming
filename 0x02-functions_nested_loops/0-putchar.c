eb3b51b13d4616086290e302feb0f7da7c244071
Ismail Jamiu Babatunde <32511206+phantware@users.noreply.github.com>
2022-03-15 13:37:52 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include "main.h"
-/**
- * prints the alphabet, in lowercase, followed by a new line.
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	char letter = 'a';
 
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
+/**
+ * main - prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
+	return (0);
 }

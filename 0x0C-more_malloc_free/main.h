efd03e40dc983d77e7bc43d0244c3db887ec55b5
Ismail Jamiu Babatunde <32511206+phantware@users.noreply.github.com>
2022-04-07 15:13:13 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -7,5 +7,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 #endif

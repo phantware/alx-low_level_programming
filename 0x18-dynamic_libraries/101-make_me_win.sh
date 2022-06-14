2809ca302e9fd629b5ddd9dc2895cc2ebaa5c072
Ismail Jamiu Babatunde <32511206+phantware@users.noreply.github.com>
2022-06-14 07:01:21 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Lexxyla/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/phantware/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
